7b0dad24488e07fb0945fe567992326786021552
jova93 <89088512+jova93@users.noreply.github.com>
2022-03-05 10:36:48 +0100

    create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showMenuLinks() {
+  var x = document.getElementById("menuLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
